2a8e8fe746494275529ad4ec31a5572b4f61f804
Varintorn SIthisint <76083506+VarinPond@users.noreply.github.com>
2024-07-23 10:27:02 +0700

    Update __init__.py

--- networks/__init__.py
+++ networks/__init__.py
@@ -4,3 +4,7 @@ def get_model(name, max_iter, num_classes=2, pretrained=True, normed_fc=True, us
         from .resnet_lgt import resnet18
         model = resnet18(max_iter=max_iter, num_classes=num_classes, pretrained=pretrained, normalized=normed_fc, use_bias=use_bias, simsiam=simsiam)
         return model
+    if name == "ResNet50_lgt":
+        from .resnet_lgt import resnet50
+        model = resnet50(max_iter=max_iter, num_classes=num_classes, pretrained=pretrained, normalized=normed_fc, use_bias=use_bias, simsiam=simsiam)
+        return model
